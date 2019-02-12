18b47657423a05165e6215ddb8db5b516f128f45
André Elmoznino Laufer <andre.elmoznino@CLM-2D3JGH5.local>
2019-02-12 10:29:47 +0100

    refactor: rename navigation to header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { NgModule } from '@angular/core'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { StartComponent } from './start/start.component'
-import { NavigationComponent } from './shared/navigation/navigation.component'
 import { GraphQLModule } from './graphql.module'
 import { HttpClientModule } from '@angular/common/http'
 import { CommonModule } from '@angular/common'
+import { HeaderComponent } from './core/header/header.component'
 
 @NgModule({
-  declarations: [AppComponent, StartComponent, NavigationComponent],
+  declarations: [AppComponent, StartComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavigationComponent } from './navigation.component';
+import { HeaderComponent } from './header.component';
 
-describe('NavigationComponent', () => {
-  let component: NavigationComponent;
-  let fixture: ComponentFixture<NavigationComponent>;
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavigationComponent ]
+      declarations: [ HeaderComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavigationComponent);
+    fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
